47fd1737ad45ee6b13bb74b80ffe21962df69aa2
Bomanaziba Ogoni <bomana.ogoni@hotmail.com>
2019-05-08 09:04:28 +0100

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers {
+    [Route ("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase {
+        private readonly DataContext _content;
+
+        public ValuesController (DataContext _content) {
+
+            this._content = _content;
+
+        }
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> GetValues () {
+
+            var values = await this._content.Values.ToListAsync();
+
+            return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet ("{id}")]
+        public async Task<IActionResult> GetValue (int id) {
+
+            var value = await this._content.Values.FirstOrDefaultAsync(x=>x.Id == id);
+
+            return Ok(value);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post ([FromBody] string value) { }
+
+        // PUT api/values/5
+        [HttpPut ("{id}")]
+        public void Put (int id, [FromBody] string value) { }
+
+        // DELETE api/values/5
+        [HttpDelete ("{id}")]
+        public void Delete (int id) { }
+    }
+}
