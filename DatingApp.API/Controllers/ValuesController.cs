31a501b4decd4c3e20311bfb3e8c5d1c7fadb08c
Bomanaziba Ogoni <bomana.ogoni@hotmail.com>
2019-05-20 16:57:18 +0100

    End of Section 4 register and sign

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -19,7 +19,7 @@ namespace DatingApp.API.Controllers {
             this._content = _content;
         }
 
-        // GET api/values
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetValues () 
         {
